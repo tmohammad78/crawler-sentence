c5b238283ba5019b3775663e8225a5ec46caad55
tmohammad78 <mohammadtaheri506@gmail.com>
2022-05-27 19:11:54 +0430

    fix slice and add echo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Crawler
+This is sample application for crawling a english website.
+
+## Demand
+I need a crawler to extract sentences of english by specific words and each day I should see 5 or more sentence of that word randomly and it should not be duplicated and i want to make this as a web app to use this on my learning english process :)) 
+
+### Example 
+I learned this words today : fed up , go through , imitate , nonetheless
+and I want 20 sentence totally to review these words , For each word i should have 5 sentece
--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 
+	"github.com/labstack/echo/v4"
 	"golang.org/x/net/html"
 )
 
@@ -88,62 +90,125 @@ func getHtmlPage(webPage string) (string, error) {
 	return string(body), nil
 }
 
+func generateURL(url string,word string) string{
+	newUrl := url
+	if(strings.Contains(word," ")){
+		words := strings.Split(word," ")
+		newUrl += "/" + words[0] + "%20" + words[1]+ ".html"
+	}
+	return newUrl
+}
+
+func generateSentense(tokenizer *html.Tokenizer) string {
+	ttt := ""
+	digitDot := regexp.MustCompile(`\d*\.`)
+	previousStartTokenTest := tokenizer.Token()
+	loopDomTest :
+	for {
+		tokenType := tokenizer.Next()
+		switch {
+			case tokenType == html.ErrorToken:
+				break loopDomTest
+			case tokenType == html.StartTagToken:
+				previousStartTokenTest = tokenizer.Token()
+			case tokenType == html.TextToken :
+				mainWord := ""
+				if previousStartTokenTest.Data == "script" {
+					continue
+				}
+				if previousStartTokenTest.Data == "em" {
+					mainWord = strings.TrimSpace(html.UnescapeString(string(tokenizer.Text())))
+				}
+				TxtContent := strings.TrimSpace(html.UnescapeString(string(tokenizer.Text())))
+				TxtContent = digitDot.ReplaceAllString(TxtContent,"${1}")
+				// fmt.Printf("%s\n", TxtContent)
+				// fmt.Printf(previousStartTokenTest.Data)
+				if len(mainWord) > 0 {
+					TxtContent += " " + mainWord + " "
+				}
+				ttt += TxtContent
+				// if len(TxtContent) > 0 {
+				// 		// fmt.Printf("%s\n", TxtContent)
+				// }			
+			}
+		}
+		return ttt
+}
+
+func generatedObject( wantedWord string,ttt string ) sentencesList {
+	sentencesSlice := sentencesList{}
+	for index,item:= range strings.Split(ttt,"."){
+		sentencesSlice[wantedWord] = append(sentencesSlice[wantedWord], sentence{id:index,shown:false,text:item})
+	}
+	return sentencesSlice
+}
+
+func generateHTMLDOM(data string) *html.Node {
+	doc, err := html.Parse(strings.NewReader(data))
+
+	if err != nil {
+			log.Fatal(err)
+	}
+	return doc
+}
+
+var (
+	seq   = 1
+)
+
 type sentence  struct { 
-	id uint8
+	id int
 	shown bool
 	text string
 }
+
+type word struct{
+	ID int `json:"id"`
+	Word string `json:"word"`
+}
+
 type sentencesList map[string][]sentence
 
 func main() {
-		var url = "https://sentencedict.com/Fed%20up.html"
-		data , err := getHtmlPage(url)
-		sentencesSlice := sentencesList{}
-		sentencesSlice["fed up"] = append(sentencesSlice["fed up"], sentence{id:12,shown:false,text: "test"})
+		var url = "https://sentencedict.com"
+		e := echo.New()
+		result := sentencesList{}
+		wantedWord :=""
+		e.GET("/",func (c echo.Context) error {
+			return c.String(http.StatusOK,"Hello")
+		})
+		e.POST("/word", func (c echo.Context) error{
+			w := &word{
+				ID: seq,
+			}
+			if err := c.Bind(w);err != nil {
+				return err
+			}
+			wantedWord = c.FormValue("word")
+			
+			return c.JSON(http.StatusOK, "Done")
+		})
+
+		e.GET("/word",func (c echo.Context) error  {
+			return c.JSON(http.StatusOK, result)
+		})
+		e.Logger.Fatal(e.Start(":1323"))
+		data , err := getHtmlPage(generateURL(url,wantedWord))
 
 		if err != nil {
 			log.Fatal(err)
 		}
-		doc, err := html.Parse(strings.NewReader(data))
 
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    tag := getElementById(doc, "all")
-    output := renderNode(tag)
-		digitDot := regexp.MustCompile(`\d*\.`)
+		output := renderNode(getElementById(generateHTMLDOM(data), "all"))
 		tokenizer := html.NewTokenizer(strings.NewReader(output))
-		previousStartTokenTest := tokenizer.Token()
-		loopDomTest :
-		for {
-			ttt := ""
-			tokenType := tokenizer.Next()
-			switch {
-				case tokenType == html.ErrorToken:
-					break loopDomTest
-				case tokenType == html.StartTagToken:
-					previousStartTokenTest = tokenizer.Token()
-				case tokenType == html.TextToken :
-					mainWord := ""
-					if previousStartTokenTest.Data == "script" {
-						continue
-					}
-					if previousStartTokenTest.Data == "em" {
-						mainWord = strings.TrimSpace(html.UnescapeString(string(tokenizer.Text())))
-					}
-					TxtContent := strings.TrimSpace(html.UnescapeString(string(tokenizer.Text())))
-					TxtContent = digitDot.ReplaceAllString(TxtContent,"${1}")
-					// fmt.Printf(previousStartTokenTest.Data)
-					if len(mainWord) > 0 {
-						TxtContent += mainWord
-					}
-					ttt += TxtContent
-					if len(TxtContent) > 0 {
-							// fmt.Printf("%s\n", TxtContent)
-					}
-					fmt.Printf("%s\n", ttt)
-			}
+		ttt:= generateSentense(tokenizer)
+		result = generatedObject(wantedWord,ttt)
+		file , err := os.Create("wtf"); 
+		if err != nil {
+			panic(err)
 		}
+		defer file.Close()
+		file1, _ := json.MarshalIndent(result, "", " ")
+		_ = ioutil.WriteFile(file.Name(), file1, 0644)
 
 }
